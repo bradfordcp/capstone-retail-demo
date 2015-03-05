d72292689b52863575373be66d6665cd1d486f47
Joaquin Casares <joaquin@datastax.com>
2015-03-04 23:17:18 -0800

    enable spark. clone the correct repo

--- vagrant/clone-cornerstone.sh
+++ vagrant/clone-cornerstone.sh
@@ -4,4 +4,4 @@
 sudo mkdir -p /cornerstone
 sudo chown -R $(whoami):$(whoami) /cornerstone
 
-git clone https://github.com/datastax-demos/cornerstone.git /cornerstone
+git clone https://github.com/datastax-demos/capstone-retail-demo.git /cornerstone
